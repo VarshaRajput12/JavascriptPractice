2723eb8fa037879872164e1c35e320ad585b9fd2
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-15 13:40:04 +0530

    JSpractice

---
+++ polyfills/filter.js
@@ -0,0 +1,26 @@
+//polyfill for filter
+
+let arr = [2, 5, 8, 10, 24, 13, 15];
+
+// let newArr = arr.filter((ele, i, arr)=>{
+//   return ele % 2 === 0
+// })
+
+// console.log(newArr)
+
+Array.prototype.myFilter = function (callback) {
+  let newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArr.push(this[i]);
+    }
+  }
+
+  return newArr;
+};
+
+let newArr = arr.myFilter((ele, i, arr) => {
+  return ele % 2 !== 0;
+});
+
+console.log(newArr);

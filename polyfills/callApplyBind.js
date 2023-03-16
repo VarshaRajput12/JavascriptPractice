5caf6fa8cdcf58086b7b1d2211380f7e6d7a9b26
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-16 15:13:01 +0530

    JSpractice

---
+++ polyfills/callApplyBind.js
@@ -0,0 +1,42 @@
+let obj = {
+  a: 20,
+  b: 30,
+};
+
+function tester(a, b) {
+  return `a: ${this.a} b: ${this.b} | other a: ${a} b: ${b}`;
+}
+// let bindObj = tester.bind(obj, 100, 200);
+// console.log(tester.call(obj, 50, 80));
+// console.log(tester.apply(obj, [50, 80]));
+// console.log(bindObj());
+
+// polyfills;
+
+//bind
+
+Function.prototype.myBind = function (scope, ...args) {
+  scope.value = this;
+  return function () {
+    return scope.value(...args);
+  };
+};
+
+//call
+
+Function.prototype.myCall = function (scope, ...args) {
+  scope._this = this;
+  return scope._this(...args);
+};
+
+// apply
+
+Function.prototype.myApply = function (scope, args) {
+  scope._this = this;
+  return scope._this(...args);
+};
+
+let bindObj2 = tester.myBind(obj, 100, 200);
+console.log(tester.myCall(obj, 50, 80));
+console.log(tester.myApply(obj, [50, 80]));
+console.log(bindObj2());

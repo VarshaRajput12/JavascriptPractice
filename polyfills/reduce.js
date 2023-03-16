6d54069f2677aabc8f2c7df0e051af6a19b9883c
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-16 11:45:12 +0530

    JSpractice

---
+++ polyfills/reduce.js
@@ -0,0 +1,23 @@
+// reduce polyfill 
+let arr = [2, 2, 1, 5];
+
+// let sum = arr.reduce((acc,pre)=>{
+//     return acc + pre
+// },0)
+
+// console.log(sum)
+
+Array.prototype.myReduce = function (callback, initialValue = null) {
+  let accumlator = initialValue;
+
+  for (let i = 0; i < this.length; i++) {
+    accumlator = callback(accumlator, this[i], i, this);
+  }
+  return accumlator;
+};
+
+let sum = arr.myReduce((acc, pre) => {
+  return acc + pre;
+}, 0);
+
+console.log(sum);

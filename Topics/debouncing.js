b9f43c06ad374cd4cdc83aff0fe63d8a7e7bab38
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-18 19:42:53 +0530

    practice

---
+++ Topics/debouncing.js
@@ -0,0 +1,15 @@
+let count = 0;
+function getData() {
+  console.log(`function called  ${count++}`);
+}
+function myDebounce(callback, delay) {
+  let interval;
+  return function () {
+    if (interval)clearInterval(interval);
+    
+      interval = setInterval(() => {
+      callback();
+    }, delay);
+  };
+}
+const betterFunction = myDebounce(getData, 2000);

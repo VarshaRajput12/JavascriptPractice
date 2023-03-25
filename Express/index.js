43f449cd010492d1b96add77bc7b8d1374edf346
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-25 18:01:03 +0530

    practice

--- Express/index.js
+++ Express/index.js
@@ -1,24 +1,23 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const app = express();
-const path = require("path")
+const path = require("path");
 const port = 4000;
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname + "/index.html"))
-})
-app.post("/api/v1/register", (req, res)=>{
-    const userName = req.body.name;
-    const userPassword = req.body.password;
-    res.json({
-        success: true,
-    })
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
-
+app.post("/api/v1/register", (req, res) => {
+  const userName = req.body.name;
+  const userPassword = req.body.password;
+  res.json({
+    success: true,
+  });
+});
 
 app.listen(port, () => {
   console.log(`server is working: ${port}`);

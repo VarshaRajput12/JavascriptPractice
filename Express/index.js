9c95a5e6eeb74053195c17a69b66dbeb2a548142
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-25 17:59:33 +0530

    practice

---
+++ Express/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const app = express();
+const path = require("path")
+const port = 4000;
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
+app.post("/api/v1/register", (req, res)=>{
+    const userName = req.body.name;
+    const userPassword = req.body.password;
+    res.json({
+        success: true,
+    })
+});
+
+
+app.listen(port, () => {
+  console.log(`server is working: ${port}`);
+});
---
+++ script.js
@@ -0,0 +1,31 @@
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "68d16905e4msh7755e13402f176ep1f5f18jsn99ccb3d82956",
+//     "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+//   },
+// };
+
+// fetch(
+//   "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+
+let obj1 = {a: 10, b: 5, c: 3}
+let obj2 = {a: 15, b: 5, c: 3, e: 8}
+
+// output = {b: 5, c: 3}
+
+// let keysOfObj1 = Object.keys(obj1)
+// let keysOfObj2 = Object.keys(obj2)
+
+// console.log(keysOfObj1)
+// console.log(keysOfObj2)
+
+for(let keys in obj1){
+  console.log(obj2[keys])
+}
